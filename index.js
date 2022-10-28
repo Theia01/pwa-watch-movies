9316b8981770edd79a9f0dcd1fe618c21748b891
Axelle <guinaudeau.ion1516@gmail.com>
2022-10-28 15:04:59 +0200

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("searchForm");
+const searchInput = document.getElementById("search");
+const result = document.getElementById("result");
+
+let search = "";
+let movies = [];
+
+const fetchMovies = async () => {
+  movies = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=6179d850024219ef492286f790e682e1&query=${search}`
+  ).then((res) => res.json());
+  console.log(movies);
+};
+
+const moviesDisplay = async () => {
+  await fetchMovies();
+
+  movies.results.length = 12;
+
+  result.innerHTML = movies.results
+    .map(
+      (movie) =>
+        `
+        <div class="col-12 col-md-6 col-lg-4">
+        <div class="card">
+            <img class="card-img-top" src="https://image.tmdb.org/t/p/w500${movie.poster_path}"></img>
+            <div class="card-body">
+                <h5 class="card-title">${movie.original_title}</h5>
+                <p class="card-text">${movie.overview}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+    `
+    )
+    .join("");
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  search = searchInput.value;
+  moviesDisplay();
+});
