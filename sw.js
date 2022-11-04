3a73884ba0b2d7d3bc94399d8e409fa96dc8f59a
Axelle <guinaudeau.ion1516@gmail.com>
2022-11-04 10:09:36 +0100

    add cache to app

--- sw.js
+++ sw.js
@@ -1,12 +1,44 @@
+// mise en cache avant de lancer l'activation
+var CACHE_NAME = 'watch-patata-cache-v1';
+var urlsToCache = [
+    '/',
+    '/style.css',
+    '/index.js',
+]
+
+// intall
 self.addEventListener('install', evt => {
+    // Perform install steps
+    evt.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+
     console.log("service worker has been installed!");
 })
 
+// activation
 self.addEventListener('activate', evt => {
-    console.log("service worker has been installed!");
+    // nettoyer les anciens fichiers pour un cache propre et a jour :)
+    evt.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhiteList.indexOf(cacheName) === -1){
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+
+    console.log("service worker has been activate!");
 })
 
-//declenche lorsqu une requete HTTP est emise par l application
+// declenche lorsqu une requete HTTP est emise par l application
 // permet d intercepter des requetes et d y repondre de façon personnalisee
 self.addEventListener('fetch', evt => {
     console.log("Ressource récupéreée" + evt.request.url);
