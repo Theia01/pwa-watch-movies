9495ae84b0ff09c9f02ee62b3100712fe6515022
Axelle <guinaudeau.ion1516@gmail.com>
2022-11-04 09:54:28 +0100

    add fetch to service workers

--- sw.js
+++ sw.js
@@ -5,3 +5,9 @@ self.addEventListener('install', evt => {
 self.addEventListener('activate', evt => {
     console.log("service worker has been installed!");
 })
+
+//declenche lorsqu une requete HTTP est emise par l application
+// permet d intercepter des requetes et d y repondre de façon personnalisee
+self.addEventListener('fetch', evt => {
+    console.log("Ressource récupéreée" + evt.request.url);
+})
